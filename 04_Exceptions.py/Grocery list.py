bc309b91416b4d01c4b389db58e8870508cfbd7f
jfNatanahel <natanahel.jf@gmail.com>
2024-01-16 01:29:42 -0300

    revisado

--- 04_Exceptions.py/Grocery list.py
+++ 04_Exceptions.py/Grocery list.py
@@ -1,9 +1,10 @@
 #####-> LISTA DE COMPRAS <-#####
 """
-Suponga que tiene la costumbre de hacer una lista de los articulos que necesita en el supermcado.
-Solicitar elementos al usuario, uno por linea, hasta que el usuario ingrese control-d. Luege genere la lista de compras del usuario en
-mayusculas, ordenadas alfabeticamente por articulo, anteponiendo cada linea con el numero de veces que el usuario ingreso ese articulo.
-No es necesario pluralizar los elementos. Trate la entrada del usuario sin distinguir entre mayusculas o minusculas."""
+Suponga que tiene la costumbre de hacer una lista de los articulos que necesita en el supermcado...
+Solicitar elementos al usuario, uno por linea, hasta que el usuario ingrese control-d. Luege genere 
+la lista de compras del usuario en mayusculas, ordenadas alfabeticamente por articulo, anteponiendo
+cada linea con el numero de veces que el usuario ingreso ese articulo. No es necesario pluralizar los
+elementos. Trate la entrada del usuario sin distinguir entre mayusculas o minusculas."""
 
 
 #Entrada del usuario.
@@ -12,5 +13,19 @@ while True:
     elementos=str(input("elementos: "))
     if elementos.lower()=="control -d":
         break
-    lista_compras[elementos.title()]=lista_compras.get(elementos,0)+1
-print(lista_compras)
+    
+#Verificar si el elemento esta en el dic
+    if elementos.title() in lista_compras:
+        lista_compras[elementos.title()]+=1
+
+#Si no se encuentra agregarlo, con el valor 1.
+    else:
+        lista_compras[elementos.title()]=1
+
+#Ordenar la lista alfabeticamente
+#items= crear una tupla
+lista_ordenada=dict(sorted(lista_compras.items()))    
+
+#Otra manera de recorrer un dict.
+for keys,value in lista_ordenada.items():
+   print(f"{value} {keys}")
--- borrador.py
+++ borrador.py
@@ -1,27 +1,16 @@
-menu = {
-    "Baja Taco": 4.24,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 9.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
-
-costo_total = 0
+lista_compras = {}
 
 while True:
-    articulo = input("Ingresar artículo a comprar: ")
+    elementos = str(input("Elemento: "))
     
-    if articulo.lower() == "control -d":
+    if elementos.lower() == "control -d":
         break
-    else:
-        try:
-            costo_total += menu[articulo.title()]
-            print("Costo total hasta el momento: ${:.2f}".format(costo_total))
-        except KeyError:
-            print("Artículo no encontrado en el menú. Por favor, ingrese un artículo válido.")
 
-print("Costo total de la orden: ${:.2f}".format(costo_total))
+    # Verificar si el elemento ya está en la lista
+    if elementos.title() in lista_compras:
+        lista_compras[elementos.title()] += 1
+    else:
+        # Si no está en la lista, agregarlo con valor 1
+        lista_compras[elementos.title()] = 1
+
+print(lista_compras)
