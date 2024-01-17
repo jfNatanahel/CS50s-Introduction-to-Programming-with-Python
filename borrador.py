aee95ed08acabe93ba7b5013dbca6cf6e42e485b
jfNatanahel <natanahel.jf@gmail.com>
2024-01-17 15:23:56 -0300

    revisado

--- borrador.py
+++ borrador.py
@@ -1,16 +1,17 @@
-lista_compras = {}
-
-while True:
-    elementos = str(input("Elemento: "))
-    
-    if elementos.lower() == "control -d":
-        break
-
-    # Verificar si el elemento ya está en la lista
-    if elementos.title() in lista_compras:
-        lista_compras[elementos.title()] += 1
-    else:
-        # Si no está en la lista, agregarlo con valor 1
-        lista_compras[elementos.title()] = 1
-
-print(lista_compras)
+mes=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+busqueda=mes.index("April")
+print(busqueda+1)
+#print(mes.index("April"))
